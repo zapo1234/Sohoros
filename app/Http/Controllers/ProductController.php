65f2169ec856fd12bc3b2557a1cc04d3faea0b14
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2021-09-12 21:32:41 +0200

    Add files via upload

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use \Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use App\Categories;
+use App\Repository\Categories\CategoriesRepositoryInterface;
+
+
+class ProductController extends Controller
+{
+
+  /*
+ *
+ *@ var CategoriesRepositoryInterface
+ *
+ */
+
+   private $categories;
+
+    /*
+ *
+ *@ var CategoriesController
+ *  constructor
+ *
+ */
+
+  public function __construct(CategoriesRepositoryInterface $categories)
+
+  {
+      $this->categories = $categories;
+  }
+
+
+    public function index(){
+
+     // recupere la liste des categeoris
+       $categoris = $this->categories->getAll();
+
+       return view('product.product', compact('categoris'));
+    }
+
+}
